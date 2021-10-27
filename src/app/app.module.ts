83b52ad7717e8d3eb3ff6f5e759a7315ec2d09d4
Kevin Maxi <kevin.maxi@ucuenca.edu.ec>
2021-10-27 18:31:28 -0500

    Add pokemon-form and pokemon-list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonListComponent,
+    PokemonFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
