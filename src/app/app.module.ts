70897220791572f234cdea01185c52c15d673d63
Kevin Maxi <kevin.maxi@ucuenca.edu.ec>
2021-10-27 20:39:51 -0500

    Add visual design of pokemon-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { AppComponent } from './app.component';
 import {FormsModule} from "@angular/forms";
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -16,7 +21,13 @@ import { PokemonFormComponent } from './pokemon-form/pokemon-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
